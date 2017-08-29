776008dffcec9d27bad2520261087b4238ba4af7
abhinav-gu <abhinav-gu@hcl.com>
2017-08-29 12:00:47 +0530

    Basic HTTP server is created and export that module to index.js

---
+++ server.js
@@ -0,0 +1,35 @@
+// HTTP SERVER with file name server.js
+/*var http = require("http");
+
+http.createServer(function(request, response){
+	response.writeHead(200, { "content-type" : "text/plain"});
+	response.write("Hello World");
+	response.end();
+}).listen(8888);
+*/
+/* We could also write our code as 
+
+var http = = require("http");
+var server = http.createServer();
+server.listen(8888);
+
+*/
+
+// We could also achieve the same by refactoring our code to :
+
+var http = require("http");
+
+function start(){
+function onRequest(request, response){
+	console.log("Request Received.")
+	response.writeHead(200, { "content-type" : "text/plain"});
+	response.write("Hello World");
+	response.end();
+}
+http.createServer(onRequest).listen(8888);
+console.log("Server Has Started.");
+	
+}
+exports.start = start;
+
+// wht are we doing it that way
